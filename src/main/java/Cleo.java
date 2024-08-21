6d1ab83ac3522b1b3a1edef19bd7e013661be5dc
verakohh <vrkwjacad@gmail.com>
2024-08-21 17:18:46 +0800

    mark as done, unmark as done, and no tasks yet

--- src/main/java/Cleo.java
+++ src/main/java/Cleo.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 public class Cleo {
     public static void main(String[] args) {
@@ -12,7 +13,8 @@ public class Cleo {
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        String[] tasks = new String[100];
+
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while(true) {
@@ -25,12 +27,33 @@ public class Cleo {
             } else if (userInput.equalsIgnoreCase("hi") || userInput.equalsIgnoreCase("hello")) {
                 System.out.println("Cleo: Hi there! How can I help you today?:)");
             } else if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("Cleo: Here are your tasks:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                //LIST OF TASKS
+                if (taskCount == 0) {
+                    System.out.println("No tasks yet!");
+                } else {
+                    System.out.println("Cleo: Here are your tasks:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + "." + tasks[i]);
+                    }
+                }
+            } else if (userInput.startsWith("mark ")) {
+                //MARK AS DONE
+                int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
+                if (taskNumber >= 0 && taskNumber < taskCount) {
+                    tasks[taskNumber].markAsDone();
+                    System.out.println("Cleo: Great job! I've marked this task as done:");
+                    System.out.println("    " + tasks[taskNumber]);
+                }
+            } else if (userInput.startsWith("unmark ")) {
+                //UNMARK AS DONE
+                int taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
+                if (taskNumber >= 0 && taskNumber < taskCount) {
+                    tasks[taskNumber].unmarkAsDone();
+                    System.out.println("Cleo: Okay! I've unmarked this task as not done yet:");
+                    System.out.println("    " + tasks[taskNumber]);
                 }
             } else {
-                tasks[taskCount] = userInput;
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
                 System.out.println("Cleo: added " + userInput);
             }
@@ -38,3 +61,4 @@ public class Cleo {
         scanner.close();
     }
 }
+
