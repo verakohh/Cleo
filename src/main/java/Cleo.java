c30e7f65c5601a61d661c0145db730a547d70c1c
verakohh <vrkwjacad@gmail.com>
2024-08-21 16:14:46 +0800

    Rename main class and add greeting

---
+++ src/main/java/Cleo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Cleo {
+    public static void main(String[] args) {
+        String logo = "  /)/)  \n"
+                + " ( •.•)\n"
+                + " (  - )♡\n";
+        System.out.println("Good day! I’m Cleo, your personal assistant,\n" +
+                "What’s on the agenda today?\n"
+                + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while(true) {
+            System.out.print("You: ");
+            userInput = scanner.nextLine();
+
+            if(userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Cleo: Goodbye, hope to see you again soon! :)");
+                break;
+            } else {
+                System.out.println("Okay!");
+
+            }
+        }
+    }
+}
